2d1a5fbd0a8a34802a04f00b9db619dff181e6b4
Nick <wecoding@126.com>
2019-08-29 21:41:41 +0800

    Initial commit

---
+++ kotlin-basic/src/main/kotlin/com/codingdenick/kotlin/basic/Hello.kt
@@ -0,0 +1,8 @@
+package com.codingdenick.kotlin.basic
+
+fun add(a: Int, b: Int) = a + b
+
+fun main(args: Array<String>){
+    println("Hello, Kotlin!")
+    println(add(1, 2))
+}
---
+++ kotlin-basic/src/test/kotlin/com/codingdenick/kotlin/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.codingdenick.kotlin
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
